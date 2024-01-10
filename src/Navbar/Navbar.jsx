fab17a45e0a5379ce501ad565dd28df899de406e
jannatun-nur <jannatunur03@gmail.com>
2024-01-10 10:07:57 +0600

    navbar 80%

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -11,16 +11,16 @@ const Navbar = () => {
         <div>
           <div className="navbar bg-rose-500 text-white rounded-xl">
   <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className=" lg:hidden">
+  <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2  rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-rose-500 rounded-box w-52">
         {links}
       </ul>
     </div>
-    <p className=' text-sm lg:textxl font-semibold lg:pr-2'>Fruit Basket</p>
-    <img className='h-10 lg:w-10' src={logo} alt="" />
+    <p className=' text-sm lg:textxl font-semibold lg:pr-2 pr-1'>Fruit Basket</p>
+    <img className='h-8 w-8 lg:h-10 lg:w-10' src={logo} alt="" />
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
@@ -28,7 +28,7 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
-<button className='px-4 py-1 rounded-lg text-white hover:bg-white hover:text-rose-500 text-xl'>Sign In</button>
+<button className='px-2 py-0 text-sm lg:px-4 lg:py-1 rounded-lg text-white hover:bg-white hover:text-rose-500 lg:text-xl'>Sign In</button>
   </div>
 </div>
         </div>

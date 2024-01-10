23ee8b7773dfb1af6880c11bc9b4dfa4ec03cfbe
jannatun-nur <jannatunur03@gmail.com>
2024-01-10 15:29:04 +0600

    nutrition 60%

--- src/Nutrition/Card.jsx
+++ src/Nutrition/Card.jsx
@@ -1,14 +1,16 @@
 
 
 const Card = ( {vitamins}) => {
-    const {name} = vitamins;
+    const {image, vitamin, name ,description} = vitamins;
     return (
-        <div>
-            <div>
-
+        <div className="bg-rose-600 flex justify-around">
+            <div className="">
+                <img className="h-10 w-10 lg:h-32 lg:w-32 " src={image} alt="image" />
+                <p>{name}</p>
+                <p>{vitamin}</p>
             </div>
-            <div>
-                
+            <div className="w-1/2">
+               <p className="w-full"> {description} </p>
             </div>
         </div>
     );
--- src/Nutrition/Nutrition.jsx
+++ src/Nutrition/Nutrition.jsx
@@ -18,7 +18,7 @@ const Nutrition = () => {
            <div className="mx-auto w-2/9 lg:w-3/12 mt-3 lg:mt-6">
            <p className="px-2 py-1 text-sm lg:px-4 lg:py-4 lg:text-2xl bg-rose-500 text-white">Nutrition Information</p>
            </div>
-            <div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
                 {
                     vitamin.map( vitamins =><Card key={vitamins._id} vitamins={vitamins}></Card>)
                 }

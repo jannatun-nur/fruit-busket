a0e9016783afee3e0bf3905f7d50d9303d592d99
jannatun-nur <jannatunur03@gmail.com>
2024-01-10 15:36:26 +0600

    nutrition 100%

--- src/Nutrition/Card.jsx
+++ src/Nutrition/Card.jsx
@@ -3,14 +3,14 @@
 const Card = ( {vitamins}) => {
     const {image, vitamin, name ,description} = vitamins;
     return (
-        <div className="bg-rose-600 flex justify-around">
+        <div className="bg-rose-200 flex justify-around lg:mt-6 rounded-lg shadow-xl">
             <div className="">
-                <img className="h-10 w-10 lg:h-32 lg:w-32 " src={image} alt="image" />
-                <p>{name}</p>
-                <p>{vitamin}</p>
+                <img className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 " src={image} alt="image" />
+                <p className="lg:text-2xl font-bold text-gray-800">{name}</p>
+                <p className="lg:text-2xl font-bold text-gray-800">{vitamin}</p>
             </div>
             <div className="w-1/2">
-               <p className="w-full"> {description} </p>
+               <p className="lg:text-xl text-gray-800"> {description} </p>
             </div>
         </div>
     );

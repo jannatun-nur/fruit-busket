309f0d8ff2a641a2f1429e855fcfac8634773e38
jannatun-nur <jannatunur03@gmail.com>
2024-01-11 09:50:49 +0600

    nutritions 60%

--- src/Nutritions/Nutritions.jsx
+++ src/Nutritions/Nutritions.jsx
@@ -1,7 +1,8 @@
 const Nutritions = () => {
   return (
-    <div>
-      <div className="bg-yellow-200 flex justify-around lg:mt-6 rounded-lg shadow-xl">
+   <div>
+     <div className="grid grid-cols-2 gap-2">
+      <div className="bg-amber-400 flex justify-around lg:mt-6 rounded-lg shadow-xl">
         <div className="">
           <img
             className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 "
@@ -56,13 +57,13 @@ const Nutritions = () => {
         </div>
         <div className="w-1/2">
           <p className="lg:text-xl text-gray-800">
-            Mangos are high in vitamins, particularly vitamin C. A one-cup
-            serving provides 66% of your daily value. You'll also benefit from a
-            healthy dose of vitamin A (11%), vitamin B6 (11%), and smaller
-            amounts of vitamin E, vitamin K, thiamin, riboflavin, folate,
-            niacin, and pantothenic acid.One cup of raw mango pieces (165g)
-            provides 99 calories, 91% of which come from carbs, 5% from protein,
-            and 5% from fat, rounding up.
+            Strawberries are very healthy, containing many antioxidants and
+            vitamins. They are also low in calories, containing only about 32
+            calories per half a cup.They’re an excellent source of vitamin C and
+            manganese and also contain decent amounts of folate (vitamin B9) and
+            potassium.Strawberries mainly consist of water (91%) and
+            carbohydrates (7.7%). They contain only minor amounts of fat (0.3%)
+            and protein (0.7%).
           </p>
         </div>
       </div>
@@ -70,25 +71,25 @@ const Nutritions = () => {
         <div className="">
           <img
             className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 "
-            src="https://i.ibb.co/YBYH5LB/mango.png "
+            src="https://i.ibb.co/rcGTtJT/banana.png"
             alt="image"
           />
-          <p className="lg:text-2xl font-bold text-gray-800">Mango</p>
-          <p className="lg:text-2xl font-bold text-gray-800">vitamin A</p>
+          <p className="lg:text-2xl font-bold text-gray-800">Banana</p>
+          <p className="lg:text-2xl font-bold text-gray-800">vitamin D</p>
         </div>
         <div className="w-1/2">
           <p className="lg:text-xl text-gray-800">
-            Mangos are high in vitamins, particularly vitamin C. A one-cup
-            serving provides 66% of your daily value. You'll also benefit from a
-            healthy dose of vitamin A (11%), vitamin B6 (11%), and smaller
-            amounts of vitamin E, vitamin K, thiamin, riboflavin, folate,
-            niacin, and pantothenic acid.One cup of raw mango pieces (165g)
-            provides 99 calories, 91% of which come from carbs, 5% from protein,
-            and 5% from fat, rounding up.
+            Bananas are among the most important food crops on the
+            planet.Bananas are a healthy source of fiber, potassium, vitamin B6,
+            vitamin C, and various antioxidants and phytonutrients.Bananas are
+            mainly composed of carbs. Unripe bananas may contain decent amounts
+            of resistant starch, which functions like fiber, aiding your gut and
+            promoting healthy blood sugar levels.
           </p>
         </div>
       </div>
     </div>
+   </div>
   );
 };
 

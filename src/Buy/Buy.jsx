fa0d10d29a71208d3efcc70bfd526b01c8eef0af
jannatun-nur <jannatunur03@gmail.com>
2024-01-11 11:01:15 +0600

    edit 70%

--- src/Buy/Buy.jsx
+++ src/Buy/Buy.jsx
@@ -14,8 +14,9 @@ const Buy = () => {
     },[])
     return (
         <div>
-             
-             <div>
+
+            <p className="lg:px-5 lg:py-2 rounded-lg bg-rose-500 text-white lg:mx-auto mx-10 lg:text-2xl w-9/12 lg:w-3/12 my-2 lg:my-10">Buy your favorite</p> 
+             <div className="grid grid-cols-2 lg:grid-cols-5">
                 {
                     buy.map(buys=><BuyCard key={buys._id} buys={buys}></BuyCard>)
                 }
--- src/Buy/BuyCard.jsx
+++ src/Buy/BuyCard.jsx
@@ -1,11 +1,27 @@
+import { FaStar } from "react-icons/fa";
+import { FaRegStarHalfStroke } from "react-icons/fa6";
 
-
-const BuyCard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const BuyCard = ({ buys }) => {
+  const { image, name, price, rating, button } = buys;
+  return (
+    <div>
+      <section className="h-64 w-56 bg-white border border-rose-600 rounded-lg">
+        <img className="lg:h-32 lg:w-36 ml-5" src={image} alt="" />
+        <h1 className="text-2xl font-bold text-center text-rose-600">{name}</h1>
+        <p className="text-xl font-bold text-center text-rose-600">{price}</p>
+        <p className="flex text-center text-amber-400 ml-16">
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaStar />
+          <FaRegStarHalfStroke />
+        </p>
+        <button className="px-2 py-1 rounded-lg text-white bg-rose-600 mt-2">
+          {button}
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default BuyCard;

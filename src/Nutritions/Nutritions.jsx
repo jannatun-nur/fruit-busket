82d86ada80a5b0fed9846cc3b29b1a5893e99810
jannatun-nur <jannatunur03@gmail.com>
2024-01-11 10:10:57 +0600

    nutritions 80%

--- src/Nutritions/Nutritions.jsx
+++ src/Nutritions/Nutritions.jsx
@@ -1,19 +1,20 @@
 const Nutritions = () => {
   return (
    <div>
-     <div className="grid grid-cols-2 gap-2">
-      <div className="bg-amber-400 flex justify-around lg:mt-6 rounded-lg shadow-xl">
+    <p className="lg:px-5 lg:py-2 rounded-lg bg-rose-500 text-white lg:mx-auto mx-10 lg:text-2xl w-9/12 lg:w-3/12 my-2 lg:mt-4">Basic Nutrition Information</p>
+     <div className="grid lg:grid-cols-2 gap-2">
+      <div className="bg-amber-400 lg:flex lg:justify-around flex justify-between lg:mt-6 rounded-lg shadow-xl">
         <div className="">
           <img
-            className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 "
+            className="h-24 w-24 lg:h-32 lg:w-32 lg:mt-20 mt-28 "
             src="https://i.ibb.co/YBYH5LB/mango.png "
             alt="image"
           />
-          <p className="lg:text-2xl font-bold text-gray-800">Mango</p>
-          <p className="lg:text-2xl font-bold text-gray-800">vitamin A</p>
+          <p className=" text-sm lg:text-2xl font-bold text-gray-800">Mango</p>
+          <p className="text-sm lg:text-2xl font-bold text-gray-800">vitamin A</p>
         </div>
-        <div className="w-1/2">
-          <p className="lg:text-xl text-gray-800">
+        <div className=" w-1/2 lg:w-1/2">
+          <p className=" text-sm lg:text-xl text-gray-800">
             Mangos are high in vitamins, particularly vitamin C. A one-cup
             serving provides 66% of your daily value. You'll also benefit from a
             healthy dose of vitamin A (11%), vitamin B6 (11%), and smaller
@@ -27,14 +28,14 @@ const Nutritions = () => {
       <div className="bg-purple-500 flex justify-around lg:mt-6 rounded-lg shadow-xl">
         <div className="">
           <img
-            className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 "
+            className="h-24 w-24 lg:h-32 lg:w-32 lg:mt-20 mt-28"
             src="https://i.ibb.co/9N33bwQ/grape.png "
             alt="image"
           />
-          <p className="lg:text-2xl font-bold text-gray-800">Grape</p>
-          <p className="lg:text-2xl font-bold text-gray-800">vitamin B</p>
+          <p className=" text-sm lg:text-2xl font-bold text-gray-800">Grape</p>
+          <p className="  text-sm lg:text-2xl font-bold text-gray-800">vitamin B</p>
         </div>
-        <div className="w-1/2">
+        <div className=" w-1/2 lg:w-1/2">
           <p className="lg:text-xl text-gray-800">
             Grapes are a sweet, low-fat, and low-calorie treat. If you are
             trying to reduce your intake of processed foods or added sugars,
@@ -48,14 +49,14 @@ const Nutritions = () => {
       <div className="bg-rose-300 flex justify-around lg:mt-6 rounded-lg shadow-xl">
         <div className="">
           <img
-            className="h-10 w-10 lg:h-32 lg:w-32 lg:mt-20 "
+            className="h-24 w-24 lg:h-32 lg:w-32 lg:mt-20 mt-28 "
             src="https://i.ibb.co/GCVKdXs/straw.png"
             alt="image"
           />
-          <p className="lg:text-2xl font-bold text-gray-800">Strawberry</p>
-          <p className="lg:text-2xl font-bold text-gray-800">vitamin C</p>
+          <p className=" text-sm  lg:text-2xl font-bold text-gray-800">Strawberry</p>
+          <p className=" text-sm  lg:text-2xl font-bold text-gray-800">vitamin C</p>
         </div>
-        <div className="w-1/2">
+        <div className="  w-1/2 lg:w-1/2">
           <p className="lg:text-xl text-gray-800">
             Strawberries are very healthy, containing many antioxidants and
             vitamins. They are also low in calories, containing only about 32

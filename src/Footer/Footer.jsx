f5e18e6ba584dee97830e80bca396cdbb4565e9c
jannatun-nur <jannatunur03@gmail.com>
2024-01-21 11:35:55 +0600

    edit 60

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import logo from '../../public/assets/image/logos.png'
+import { GiRotaryPhone } from "react-icons/gi";
+import { IoIosMail } from "react-icons/io";
 
 const Footer = () => {
   return (
@@ -9,6 +11,15 @@ const Footer = () => {
         <img className='h-10 w-10 lg:h-16 lg:w-16' src={logo} alt="" />
       </section>
        
+       <section>
+        <p className='flex gap-2 text-white'>
+        <span className='pt-1'><GiRotaryPhone /> </span>+495637965
+        </p>
+        <p className='flex gap-2 text-white'>
+        <span className='pt-1'><IoIosMail /></span>+495637965
+        </p>
+        
+       </section>
       </footer>
     </div>
   );

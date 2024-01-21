90704781e68d18e24d2883a1d9f24b674baca5c1
jannatun-nur <jannatunur03@gmail.com>
2024-01-21 10:08:49 +0600

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
 
   return (
-    <div className='min-h-screen bg-white'>
+    <div className='min-h-screen'>
       <Navbar/>
       <Slider/>
      <Nutritions/>
--- src/Buy/BuyCard.jsx
+++ src/Buy/BuyCard.jsx
@@ -13,11 +13,11 @@ const BuyCard = ({ buys }) => {
 
   return (
     <div>
-      <section className="h-64 w-56 bg-white border border-rose-600 rounded-lg">
-        <img className="lg:h-32 lg:w-36 ml-5" src={image} alt="" />
+      <section className="h-48 w-36 lg:h-64 lg:w-56 bg-white border border-rose-600 rounded-lg">
+        <img className="h-20 w-20 lg:h-32 lg:w-36 ml-5" src={image} alt="" />
         <h1 className="text-2xl font-bold text-center text-rose-600">{name}</h1>
         <p className="text-xl font-bold text-center text-rose-600">{price}</p>
-        <p className="flex text-center text-amber-400 ml-16">
+        <p className="flex text-center text-amber-400 lg:ml-16 ml-8 mb-2">
           <FaStar />
           <FaStar />
           <FaStar />
